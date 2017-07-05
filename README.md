3b99af45df12136315fa150cb870ceab4d56a0c3
Helton Santos <helton.santos@nutes.ufpe.br>
2017-07-05 17:14:29 -0300

    Material

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# minicurso-SpringBoot
+#Mini Curso - Spring Boot + MySQL + JPA + RESTful
+
+O Spring Boot é uma ferramenta para o desenvolvimento de aplicações Java que utilizam o framework Spring sem que seja necessária praticamente nenhuma configuração. Ele é capaz de identificar quais as principais características da aplicação que está sendo desenvolvida e fazer automaticamente as configurações necessárias. Dessa forma, os programadores aumentam sua produtividade, focando apenas no desenvolvimento da aplicação e não na configuração das ferramentas e frameworks utilizados, que são repetidos em praticamente todos os projetos.
+
+Neste curso você poderá:
+
+-Criar aplicações e micro serviços Java com o Spring Boot.
+-Construir Web Services para comunicação com outras aplicações.
+-Conectar-se à base de dados MySQL, entre outras.
+-Fazer o mapeamento de seus Objetos Java para o Modelo Relacional com JPA.
+-Testar seus serviços com JUnit.
--- material/scripts/db_universidade_povoamento.sql
+++ material/scripts/db_universidade_povoamento.sql
@@ -1,14 +1,14 @@
 
-Insert into db_universidade.departamento (nome, codigo_departamento) values ('InformaçÁ„o e Sistemas', 'D001');
-Insert into db_universidade.departamento (nome, codigo_departamento) values ('Tecnologia da InformaçÁ„oo', 'D002');
+Insert into db_universidade.departamento (nome, codigo_departamento) values ('InformaÁ„o e Sistemas', 'D001');
+Insert into db_universidade.departamento (nome, codigo_departamento) values ('Tecnologia da InformaÁ„o', 'D002');
 
-Insert into db_universidade.professor (nome, siape, categoria, salario, departamento_id) values ('Maria', '1234', 'Adjunto 1', 1500, 1);
-Insert into db_universidade.professor (nome, siape, categoria, salario, departamento_id) values ('Pedro', '5678', 'Adjunto 2', 2000, 1);
-Insert into db_universidade.professor (nome, siape, categoria, salario, departamento_id) values ('Jo„o', '1122', 'Adjunto 1', 1500, 2);
-Insert into db_universidade.professor (nome, siape, categoria, salario, departamento_id) values ('Carlos', '5522', 'Adjunto 1', 1500, 2);
+Insert into db_universidade.professor (nome, cpf, categoria, salario, departamento_id) values ('Maria', '12345678944', 'Adjunto 1', 1500, 1);
+Insert into db_universidade.professor (nome, cpf, categoria, salario, departamento_id) values ('Pedro', '98745632155', 'Adjunto 2', 2000, 1);
+Insert into db_universidade.professor (nome, cpf, categoria, salario, departamento_id) values ('Jo„o', '11122233344', 'Adjunto 1', 1500, 2);
+Insert into db_universidade.professor (nome, cpf, categoria, salario, departamento_id) values ('Carlos', '55566677788', 'Adjunto 1', 1500, 2);
 
 Insert into db_universidade.disciplina (nome, codigo_disciplina, carga_horaria) values ('Banco de Dados', 'IF976', '60h');
-Insert into db_universidade.disciplina (nome, codigo_disciplina, carga_horaria) values ('ProgramaçÁ„o 1', 'IF992', '60h');
+Insert into db_universidade.disciplina (nome, codigo_disciplina, carga_horaria) values ('ProgramaÁ„o 1', 'IF992', '60h');
 Insert into db_universidade.disciplina (nome, codigo_disciplina, carga_horaria) values ('Fundamentos de SI', 'IF973', '75h');
 
 Insert into db_universidade.ministra (professor_id, disciplina_id, periodo) values (1, 3, '2013.1');
